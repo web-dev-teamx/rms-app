312ab187f9cfb927195164fd62d8bf6ad8dee02e
Inamullah Shah <89691107+inamullahshah143@users.noreply.github.com>
2022-07-02 14:40:48 +0500

    .

--- SVI-RMS/lib/ui/authentication/profile_setup/multi_step_form.dart
+++ SVI-RMS/lib/ui/authentication/profile_setup/multi_step_form.dart
@@ -3,6 +3,9 @@ import 'dart:ui';
 import 'package:cool_stepper/cool_stepper.dart';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttericon/entypo_icons.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:fluttericon/mfg_labs_icons.dart';
 
 class MultiStepForm extends StatefulWidget {
   const MultiStepForm({Key? key}) : super(key: key);
@@ -15,7 +18,7 @@ class MultiStepForm extends StatefulWidget {
 class _MultiStepFormState extends State<MultiStepForm> {
   final _formKey = GlobalKey<FormState>();
   CountryCode? countryCode;
-  static List<String> friendsList = [''];
+  static List<String> languageList = [''];
 
   TextEditingController? _nameController;
 
@@ -198,25 +201,9 @@ class _MultiStepFormState extends State<MultiStepForm> {
                       topRight: Radius.circular(16.0)),
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0,
-                    ),
-                    child: DropdownButtonFormField(
-                      isDense: true,
-                      onChanged: (value) {},
-                      items: const [
-                        DropdownMenuItem(
-                          child: Text('Recruiter Consultant'),
-                          value: 'RecruiterConsultant',
-                        ),
-                        DropdownMenuItem(
-                          child: Text('Recruiter Consultant'),
-                          value: 'RecruiterConsultant',
-                        ),
-                        DropdownMenuItem(
-                          child: Text('Recruiter Consultant'),
-                          value: 'RecruiterConsultant',
-                        ),
-                      ],
+                        horizontal: 16.0, vertical: 8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.text,
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         label: RichText(
@@ -253,35 +240,37 @@ class _MultiStepFormState extends State<MultiStepForm> {
                 height: 70,
                 child: Material(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
                       keyboardType: TextInputType.datetime,
                       decoration: InputDecoration(
-                          border: InputBorder.none,
-                          label: RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: 'Registration Date',
-                                  style: TextStyle(
-                                    color: Colors.grey.shade700,
-                                    fontSize: 16,
-                                  ),
+                        border: InputBorder.none,
+                        label: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'Registration Date',
+                                style: TextStyle(
+                                  color: Colors.grey.shade700,
+                                  fontSize: 16,
                                 ),
-                                const TextSpan(
-                                  text: ' *',
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontFeatures: [
-                                      FontFeature.enable('sups'),
-                                    ],
-                                  ),
+                              ),
+                              const TextSpan(
+                                text: ' *',
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontFeatures: [
+                                    FontFeature.enable('sups'),
+                                  ],
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                          prefixIcon: const Icon(Icons.calendar_today)),
+                        ),
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
+                        prefixIcon: const Icon(FontAwesome5.calendar_alt),
+                      ),
                     ),
                   ),
                 ),
@@ -293,12 +282,14 @@ class _MultiStepFormState extends State<MultiStepForm> {
                 height: 70,
                 child: Material(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
                       keyboardType: TextInputType.number,
                       decoration: InputDecoration(
                         border: InputBorder.none,
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
+                        prefixIcon: const Icon(FontAwesome5.slack_hash),
                         label: RichText(
                           text: TextSpan(
                             children: [
@@ -333,12 +324,14 @@ class _MultiStepFormState extends State<MultiStepForm> {
                 height: 70,
                 child: Material(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
                       keyboardType: TextInputType.text,
                       decoration: InputDecoration(
                         border: InputBorder.none,
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
+                        prefixIcon: const Icon(FontAwesome5.home),
                         label: Text(
                           'Full Registered Address',
                           style: TextStyle(
@@ -358,12 +351,14 @@ class _MultiStepFormState extends State<MultiStepForm> {
                 height: 70,
                 child: Material(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
                       keyboardType: TextInputType.text,
                       decoration: InputDecoration(
                         border: InputBorder.none,
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
+                        prefixIcon: const Icon(MfgLabs.globe_inv),
                         label: RichText(
                           text: TextSpan(
                             children: [
@@ -398,12 +393,14 @@ class _MultiStepFormState extends State<MultiStepForm> {
                 height: 70,
                 child: Material(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
                       keyboardType: TextInputType.text,
                       decoration: InputDecoration(
                         border: InputBorder.none,
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
+                        prefixIcon: const Icon(FontAwesome5.slack_hash),
                         label: Text(
                           'House Number & Street',
                           style: TextStyle(
@@ -423,12 +420,14 @@ class _MultiStepFormState extends State<MultiStepForm> {
                 height: 70,
                 child: Material(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
                       keyboardType: TextInputType.text,
                       decoration: InputDecoration(
                         border: InputBorder.none,
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
+                        prefixIcon: const Icon(Icons.location_city),
                         label: RichText(
                           text: TextSpan(
                             children: [
@@ -652,12 +651,13 @@ class _MultiStepFormState extends State<MultiStepForm> {
                 height: 70,
                 child: Material(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
                       keyboardType: TextInputType.phone,
                       decoration: InputDecoration(
                         border: InputBorder.none,
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
                         prefixIcon: const Icon(Icons.phone),
                         label: Text(
                           'Phone Number (Landline)',
@@ -740,13 +740,14 @@ class _MultiStepFormState extends State<MultiStepForm> {
                 height: 70,
                 child: Material(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
                       keyboardType: TextInputType.emailAddress,
                       decoration: InputDecoration(
                         border: InputBorder.none,
-                        prefixIcon: const Icon(Icons.alternate_email),
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
+                        prefixIcon: const Icon(Icons.email),
                         label: RichText(
                           text: TextSpan(
                             children: [
@@ -781,11 +782,13 @@ class _MultiStepFormState extends State<MultiStepForm> {
                 height: 70,
                 child: Material(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
                       decoration: InputDecoration(
                         border: InputBorder.none,
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
+                        prefixIcon: const Icon(FontAwesome5.skype),
                         label: RichText(
                           text: TextSpan(
                             children: [
@@ -829,12 +832,20 @@ class _MultiStepFormState extends State<MultiStepForm> {
                     bottomRight: Radius.circular(16.0),
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: TextFormField(
-                      decoration: const InputDecoration(
+                      decoration: InputDecoration(
                         border: InputBorder.none,
-                        label: Text('Current Based Country & Location'),
+                        prefixIconConstraints:
+                            const BoxConstraints(minHeight: 50, minWidth: 50),
+                        prefixIcon: const Icon(Icons.my_location),
+                        label: Text(
+                          'Current Based Country & Location',
+                          style: TextStyle(
+                            color: Colors.grey.shade700,
+                            fontSize: 16,
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -881,22 +892,22 @@ class _MultiStepFormState extends State<MultiStepForm> {
 
   List<Widget> getLanguages() {
     List<Widget> friendsTextFields = [];
-    for (int i = 0; i < friendsList.length; i++) {
+    for (int i = 0; i < languageList.length; i++) {
       friendsTextFields.add(Material(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: LanguagesTextFields(index: i),
-              ),
-              const SizedBox(
-                width: 16,
-              ),
-              // we need add button at last friends row
-              _addRemoveButton(i == friendsList.length - 1, i),
-            ],
-          ),
+        child: Row(
+          children: [
+            Expanded(
+              child: LanguagesFields(index: i),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            // we need add button at last friends row
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: _addRemoveButton(i == languageList.length - 1, i),
+            ),
+          ],
         ),
       ));
     }
@@ -909,9 +920,9 @@ class _MultiStepFormState extends State<MultiStepForm> {
       onTap: () {
         if (add) {
           // add new text-fields at the top of all friends textfields
-          friendsList.insert(0, '');
+          languageList.insert(0, '');
         } else {
-          friendsList.removeAt(index);
+          languageList.removeAt(index);
         }
         setState(() {});
       },
@@ -931,40 +942,59 @@ class _MultiStepFormState extends State<MultiStepForm> {
   }
 }
 
-class LanguagesTextFields extends StatefulWidget {
+class LanguagesFields extends StatefulWidget {
   final int index;
 
-  const LanguagesTextFields({Key? key, required this.index}) : super(key: key);
+  const LanguagesFields({Key? key, required this.index}) : super(key: key);
   @override
-  _LanguagesTextFieldsState createState() => _LanguagesTextFieldsState();
+  _LanguagesFieldsState createState() => _LanguagesFieldsState();
 }
 
-class _LanguagesTextFieldsState extends State<LanguagesTextFields> {
-  TextEditingController _nameController = TextEditingController();
+class _LanguagesFieldsState extends State<LanguagesFields> {
+  String? nameController;
 
   @override
   void initState() {
     super.initState();
-    _nameController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    super.dispose();
+    nameController = '';
   }
 
   @override
   Widget build(BuildContext context) {
     WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
-      _nameController.text = _MultiStepFormState.friendsList[widget.index];
+      nameController = _MultiStepFormState.languageList[widget.index];
     });
 
-    return TextFormField(
-      controller: _nameController,
-      onChanged: (v) => _MultiStepFormState.friendsList[widget.index] = v,
+    return DropdownButtonFormField(
+      items: const [
+        DropdownMenuItem(
+          child: Text('English'),
+          value: 'English',
+        ),
+        DropdownMenuItem(
+          child: Text('Spanish'),
+          value: 'Spanish',
+        ),
+        DropdownMenuItem(
+          child: Text('French'),
+          value: 'French',
+        ),
+        DropdownMenuItem(
+          child: Text('Italian'),
+          value: 'Italian',
+        ),
+        DropdownMenuItem(
+          child: Text('Russian'),
+          value: 'Russian',
+        ),
+      ],
+      onChanged: (v) =>
+          _MultiStepFormState.languageList[widget.index] = v.toString(),
       decoration: InputDecoration(
         border: InputBorder.none,
+        prefixIconConstraints:
+            const BoxConstraints(minHeight: 50, minWidth: 50),
+        prefixIcon: const Icon(Entypo.language),
         label: RichText(
           text: TextSpan(
             children: [
@@ -988,8 +1018,8 @@ class _LanguagesTextFieldsState extends State<LanguagesTextFields> {
           ),
         ),
       ),
-      validator: (v) {
-        if (v!.trim().isEmpty) {
+      validator: (String? v) {
+        if (v!.isEmpty) {
           return 'Please enter something';
         }
         return null;
